c960b4b33b15401912bb06f9ed414489805f5f51
SwapnilD-1520 <dandekarswapnil108@gmail.com>
2023-12-22 02:47:31 +0530

    Assignment - Calculate Factorial

--- src/main/java/org/example/scanner_class/Factorial.java
+++ src/main/java/org/example/scanner_class/Factorial.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Factorial {
     public static void main(String[] args) {
+
         int fact = 1;
         Scanner f1 = new Scanner(System.in);
         System.out.println(" Enter Number = ");
