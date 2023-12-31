e13d6201941ab466bea059e165dc981fa426ab95
SwapnilD-1520 <dandekarswapnil108@gmail.com>
2023-12-31 21:50:55 +0530

    Assignment - Divison Of Two Variables Using Method

---
+++ src/main/java/org/example/Method/Division.java
@@ -0,0 +1,16 @@
+package org.example.Method;
+
+public class Division {
+
+        public int Div(int x,int y){
+
+            return(x / y);
+        }
+
+         public static void main(String[] args) {
+            Division di = new Division();
+            int div = di.Div(10,2);
+            System.out.println(" Division of Number = " + div );
+
+    }
+}
