c885d44fffa5407f03b7d96d33fce982f8f16f4e
SwapnilD-1520 <dandekarswapnil108@gmail.com>
2024-01-01 23:14:51 +0530

    Assignment -Multiplication Of Variables Using Method

--- src/main/java/org/example/Method/Multiplication.java
+++ src/main/java/org/example/Method/Multiplication.java
@@ -5,7 +5,7 @@ public class Multiplication {
 
     public int multi(int m,int n){
 
-        return(m * n);
+         return(m * n);
     }
         public static void main(String[] args) {
         Multiplication m1 = new Multiplication();
